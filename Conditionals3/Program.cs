b99c244a36b85e9c09e1e8275d29fe0ee97ac5ec
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-04 12:55:30 -0300

    Update Program.cs

--- Conditionals3/Program.cs
+++ Conditionals3/Program.cs
@@ -8,9 +8,19 @@ namespace Conditionals3
 {
     class Program
     {
+        static bool isAlive = true; // true = alive, false = dead
         static void Main(string[] args)
         {
             Console.WriteLine("Conditionals3");
+
+            if (isAlive)
+            {
+                Console.WriteLine("You are alive :)");
+            }
+            else
+            {
+                Console.WriteLine("You are dead ):");
+            }
             Console.ReadKey(true);
         }
     }
